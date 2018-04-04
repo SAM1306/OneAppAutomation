cf914b1bfeb9c075d9881d188960e612bb804c6d
SAM1306 <saumiljain@ymail.com>
2018-04-04 16:15:50 -0700

    Add logger class

--- src/test/java/oneAppTests/device_list/DevicesListTests.java
+++ src/test/java/oneAppTests/device_list/DevicesListTests.java
@@ -9,6 +9,7 @@ import page.activity.PrimaryActivity;
 import page.fragment.dashboard.DashboardFragmentPage;
 import page.fragment.dashboard.SupportedDevicesFragmentPage;
 import page.view.ToolbarPage;
+import utility.Logger;
 
 public class DevicesListTests extends BaseAppiumTest {
 
@@ -37,17 +38,24 @@ public class DevicesListTests extends BaseAppiumTest {
 
     @Test
     public void listSupportedDevices() throws InterruptedException {
+        Logger.logAction("Begin: listSupportedDevices() - Test_001");
+        Logger.logStep("Click on Dashboard Button");
         primaryActivity.dashboardButton.click();
 
+        Logger.logStep("Verify Supported Devices Button is displayed");
         dashboardFragmentPage.supportedDevicesTiles.isDisplayed();
+        Logger.logStep("Click on Supported Devices Button Button");
         dashboardFragmentPage.supportedDevicesTiles.click();
 
         Thread.sleep(6000L);
+        Logger.logStep("Verify Search Bar is displayed");
         supportedDevicesFragmentPage.searchBar.isDisplayed();
 
         toolbarPage.backArrow.isDisplayed();
+        Logger.logStep("Click on Navigate Up Button");
         toolbarPage.backArrow.click();
 
+        Logger.logStep("Verify Smartthings Logo is displayed");
         dashboardFragmentPage.smartthingsLogo.isDisplayed();
     }
 }
