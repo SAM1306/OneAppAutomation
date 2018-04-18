2effa340e83d202fe2f459354bd320a0f39c014d
SAM1306 <saumiljain@ymail.com>
2018-04-18 11:29:47 -0700

    Fix Scene Tests and adding logger to SmartthingsSettings Page

--- src/test/java/page/fragment/dashboard/SmartthingsSettingsFragmentPage.java
+++ src/test/java/page/fragment/dashboard/SmartthingsSettingsFragmentPage.java
@@ -74,12 +74,16 @@ public class SmartthingsSettingsFragmentPage extends BasePage {
         ExtentTestManager.getTest().log(LogStatus.INFO, "Verify Wi-Fi and Bluetooth auto on is present and can be toggled");
         Logger.logStep("Verify Wi-Fi and Bluetooth auto on is present");
         assertTrue(wifiAndBluetoothAutoOn.isDisplayed());
+        ExtentTestManager.getTest().log(LogStatus.INFO, "Toggle Wi-Fi and Bluetooth auto on to off");
         Logger.logStep("Toggle Wi-Fi and Bluetooth auto on to off");
         wifiAndBluetoothAutoOnToggleSwitch.click();
+        ExtentTestManager.getTest().log(LogStatus.INFO, "Verify message when Wi-Fi and Bluetooth toggled off");
         Logger.logStep("Verify message when Wi-Fi and Bluetooth toggled off");
         assertTrue(wifiAndBluetoothAutoOnOffMessage.isDisplayed());
+        ExtentTestManager.getTest().log(LogStatus.INFO, "Toggle Wi-Fi and Bluetooth auto on to on");
         Logger.logStep("Toggle Wi-Fi and Bluetooth auto on to on");
         wifiAndBluetoothAutoOnToggleSwitch.click();
+        ExtentTestManager.getTest().log(LogStatus.INFO, "Verify message when Wi-Fi and Bluetooth auto on to on");
         Logger.logStep("Verify message when Wi-Fi and Bluetooth auto on to on");
         assertTrue(wifiAndBluetoothAutoOnMessage.isDisplayed());
     }
@@ -88,20 +92,25 @@ public class SmartthingsSettingsFragmentPage extends BasePage {
         ExtentTestManager.getTest().log(LogStatus.INFO, "Verify Use location information is present and can be toggled");
         Logger.logStep("Verify Use location information is present");
         assertTrue(useLocationInformation.isDisplayed());
+        ExtentTestManager.getTest().log(LogStatus.INFO, "Toggle Use location information to off");
         Logger.logStep("Toggle Use location information to off");
         useLocationInformationToggle.click();
+        ExtentTestManager.getTest().log(LogStatus.INFO, "Toggle Use location information to on");
         Logger.logStep("Toggle Use location information to on");
         useLocationInformationToggle.click();
     }
 
     public void verifyAutoUpdateController() {
-        ExtentTestManager.getTest().log(LogStatus.INFO, "Verify Use location information is present and can be toggled");
+        ExtentTestManager.getTest().log(LogStatus.INFO, "Scroll and Click to Auto Update Device Controller");
         Logger.logStep("Scroll and Click to Auto Update Device Controller");
         scrollAndClick("Auto update device controller");
+        ExtentTestManager.getTest().log(LogStatus.INFO, "Verify Off RadioButton is present");
         Logger.logStep("Verify Off RadioButton is present");
         assertTrue(autoUpdateDeviceControllerButtonOff.isDisplayed());
+        ExtentTestManager.getTest().log(LogStatus.INFO, "Verify Wi-Fi only RadioButton is present");
         Logger.logStep("Verify Wi-Fi only RadioButton is present");
         assertTrue(autoUpdateDeviceControllerButtonWiFiOnly.isDisplayed());
+        ExtentTestManager.getTest().log(LogStatus.INFO, "Verify Wi-Fi or mobile data RadioButton is present");
         Logger.logStep("Verify Wi-Fi or mobile data RadioButton is present");
         assertTrue(autoUpdateDeviceControllerButtonWiFiOrMobileData.isDisplayed());
     }
