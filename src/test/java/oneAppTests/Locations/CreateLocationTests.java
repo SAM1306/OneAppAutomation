9a0c297e7583685c534de95d07f50e5669afea35
Smartthings <andrii.malanchuk@smartthings.com>
2018-05-15 08:15:47 -0700

    Refactor Locations test

--- src/test/java/oneAppTests/Locations/CreateLocationTests.java
+++ src/test/java/oneAppTests/Locations/CreateLocationTests.java
@@ -1,6 +1,7 @@
-package oneAppTests.device_list;
+package oneAppTests.Locations;
 
 import oneAppTests.BaseAppiumTest;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import page.activity.PrimaryActivity;
@@ -10,9 +11,9 @@ import page.view.ToolbarPage;
 import utility.ExtentReports.ExtentTestManager;
 import utility.Logger;
 
-public class LocationTests extends BaseAppiumTest {
+public class CreateLocationTests extends BaseAppiumTest {
 
-    private static final String TEST_NAME = LocationTests.class.getName();
+    private static final String TEST_NAME = CreateLocationTests.class.getName();
 
     private String locationName = "Office";
 
@@ -27,32 +28,25 @@ public class LocationTests extends BaseAppiumTest {
     }
 
     @BeforeTest
-    public void setUp() {
+    public void setUp() throws InterruptedException {
         primaryActivity = new PrimaryActivity(driver);
         devicesFragmentPage = new DevicesFragmentPage(driver);
         addLocationFragmentPage = new AddLocationFragmentPage(driver);
         toolbarPage = new ToolbarPage(driver);
+        createLocation();
+    }
+
+    @AfterTest
+    public void tearDown() throws InterruptedException {
+        deleteLocation();
     }
 
     @Test(priority = 1)
-    public void createLocationTest() throws InterruptedException {
-        ExtentTestManager.getTest().setDescription("Create Location Test");
-        Logger.logAction(" \"" + TEST_NAME + "\"  Test: Create Location Test - Start");
-
-        primaryActivity.getDashboardButton();
-        primaryActivity.getDevicesButton();
-        devicesFragmentPage.verifyAndClickDeviceDropdownButton();
-        devicesFragmentPage.verifyAndClickAddLocationButton();
-        addLocationFragmentPage.verifyAndClickLocationNameTextBox();
-        addLocationFragmentPage.addLocationName(locationName);
-        toolbarPage.verifyAndClickSaveButton();
+    public void verifyLocationGotCreated() {
         devicesFragmentPage.verifyCreatedLocationIsDisplayed();
-
-        Logger.logAction(" \"" + TEST_NAME + "\"  Test: Create Location Test - End");
     }
 
-    @Test(priority = 2)
-    public void deleteLocationTest() throws InterruptedException {
+    public void deleteLocation() throws InterruptedException {
         ExtentTestManager.getTest().setDescription("Delete Location Test");
         Logger.logAction(" \"" + TEST_NAME + "\"  Test: Delete Location Test - Start");
 
@@ -66,5 +60,21 @@ public class LocationTests extends BaseAppiumTest {
         addLocationFragmentPage.verifyAndClickRemoveButton();
 
         Logger.logAction(" \"" + TEST_NAME + "\"  Test: Delete Location Test - End");
+    }
+
+    public void createLocation() throws InterruptedException {
+        ExtentTestManager.getTest().setDescription("Create Location Test");
+        Logger.logAction(" \"" + TEST_NAME + "\"  Test: Create Location Test - Start");
+
+        primaryActivity.getDashboardButton();
+        primaryActivity.getDevicesButton();
+        devicesFragmentPage.verifyAndClickDeviceDropdownButton();
+        devicesFragmentPage.verifyAndClickAddLocationButton();
+        addLocationFragmentPage.verifyAndClickLocationNameTextBox();
+        addLocationFragmentPage.addLocationName(locationName);
+        toolbarPage.verifyAndClickSaveButton();
+        devicesFragmentPage.verifyCreatedLocationIsDisplayed();
+
+        Logger.logAction(" \"" + TEST_NAME + "\"  Test: Create Location Test - End");
     }
 }
